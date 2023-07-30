037d27c4eed085c4fc92b71e248102ffc142d149
tamerat-ab <tamerat.abera18@gmail.com>
2023-07-30 19:05:22 -0400

    final_2

--- service/routes.py
+++ service/routes.py
@@ -88,7 +88,7 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
 
-      
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
